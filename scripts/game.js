55f237827e91575596a48ce45a395683cfb4a99f
Anny Devoy <annydevoyalt@gmail.com>
2024-07-06 10:12:11 +0000

    Add choices ids test

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
+    choices: ['button1', 'button2', 'button3', 'button4'],
 }
 
 module.exports = { game }; // use {} if expecting to export more than one object and function from file

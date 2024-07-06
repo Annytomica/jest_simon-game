e9242dbf94786f23e881cb6ca949f47656170e68
Anny Devoy <annydevoyalt@gmail.com>
2024-07-06 09:46:44 +0000

    Add boilerplate code for loading index.html into jsdom

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,13 @@
 /**
  * @jest-environment jsdom
  */
+
+beforeAll(() => {
+    // document.body.innerHTML = '<p id="par" ></p>'; //do this if just testing a small part of a web page
+    let fs = require('fs'); //load the 'fs' file system handling module that is built into node.js
+    let fileContents = fs.readFileSync('index.html', 'utf-8'); //saves html to variable using utf-8 character set
+    //attaching the html to the mock DOM.
+    document.open();
+    document.write(fileContents);
+    document.close();
+});

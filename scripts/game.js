eb8ad6f308a0c62fe10c7586e5c8df13280b4e12
Anny Devoy <annydevoyalt@gmail.com>
2024-07-06 10:07:12 +0000

    Add choices test

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
+    choices: [],
 }
 
 module.exports = { game }; // use {} if expecting to export more than one object and function from file
